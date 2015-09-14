7ae0cef75cbeeca5cbc4b523863a12bb5deae81e
Devin Bhushan <devinb@yahoo-inc.com>
2015-09-13 19:02:50 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Devin Bhushan on 9/13/15.
+//  Copyright (c) 2015 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
